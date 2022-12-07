765235ce6b47e3b81a9bf9155035162af372ad84
milazimlladrovci <108547568+milazimlladrovci@users.noreply.github.com>
2022-12-07 23:21:39 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Intro from './Components/Intro/Intro'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Intro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Intro.css';
+
+const intro = () => {
+  return (
+    <div className="intro">
+        <div className='i-left'>
+            <div className="i-name">
+                <span>Hy! I Am</span>
+                <span>Milazim Lladrovci</span>
+                <span>Frontend Developer of experience in web development,
+                     producting the Quality  work.
+                </span>
+            </div>
+            <button className="button i-button">Hire me</button>
+         
+
+
+        </div>
+        <div className="i-right">
+            i am right side
+        </div>
+    </div>
+  )
+}
+
+export default intro
